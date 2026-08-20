1e6658c67851c7d500f90b0ae92548a8e902ba13
Treethodsapon <pon0987682368@gmail.com>
2026-08-20 14:46:41 +0700

    First NextJS

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+    <div>
+        <h1>รายละเอียดเกี่ยวกับ</h1>
+        <li>ตรีทศพล ลือชัย</li>
+        <li>19</li>
+        <li>CSMJU</li>
+    </div>
+    );
+}
---
+++ app/courses/page.tsx
@@ -0,0 +1,68 @@
+
+
+export default function CoursesPage() {
+    type Course = { 
+    id: number; 
+    code: string; 
+    title: string; 
+    credits: number; 
+    isOpen: boolean; 
+  }; 
+ 
+  const courses: Course[] = [ 
+    { 
+      id: 1, 
+      code: "10301231", 
+      title: "Web Technology", 
+      credits: 3, 
+      isOpen: true, 
+    },
+
+    { 
+      id: 2, 
+      code: "10301232", 
+      title: "Database Systems", 
+      credits: 3, 
+      isOpen: false, 
+    },
+
+    {
+      id: 3,
+      code: "10301222",
+      title: "Algrolitem",
+      credits: 3,
+      isOpen: true,
+    },
+
+    {
+      id: 4,
+      code: "10301211",
+      title: "DiscretMath",
+      credits: 3,
+      isOpen: false,
+    },
+
+    {
+      id: 5,
+      code: "10301225",
+      title: "SoftwereEngineer",
+      credits: 2,
+      isOpen: false,
+    },
+  ]; 
+        return (
+            <div>
+                {courses.map((course, index) => (
+                    <article key={course.id}>
+                        <h2>{index+1}.{course.title}</h2>
+                        <p>รหัสวิชา: {course.code}</p>
+                        <p>{course.credits} หน่วยกิต</p>
+                        <p>{course.isOpen ? "เปิดลงทะเบียน" : "ปิดลงทะเบียน"}</p>
+                    </article>
+                    ))}
+            </div>
+        );
+
+
+
+    }
